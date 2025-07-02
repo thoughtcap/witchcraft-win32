b0035d09dd005210ffc88678d3ae9bf18bf98bc1
Jacob Gorm Hansen <jhansen@dropbox.com>
2025-07-02 16:44:16 +0200

    download weights in fp16 precision instead of fp32
    resulting file is now 209MiB uncompressed

--- downloadweights.py
+++ downloadweights.py
@@ -14,7 +14,7 @@ class XTRLinear(torch.nn.Module):
 class XTR(nn.Module):
     def __init__(self):
         super().__init__()
-        self.encoder = AutoModel.from_pretrained("google/xtr-base-en", use_safetensors=True).encoder
+        self.encoder = AutoModel.from_pretrained("google/xtr-base-en", torch_dtype=torch.float16, use_safetensors=True).encoder
         to_dense_path = hf_hub_download(repo_id="google/xtr-base-en", filename="2_Dense/pytorch_model.bin")
 
         self.encoder.linear = torch.nn.Linear(768, 128, bias=False)
@@ -24,4 +24,6 @@ class XTR(nn.Module):
         self.encoder.linear.load_state_dict(other)
 
 xtr = XTR()
-save_file(xtr.state_dict(), "xtr.safetensors")
+
+fp16_state_dict = {k: v.half().cpu() for k, v in xtr.state_dict().items()}
+save_file(fp16_state_dict, "xtr.safetensors")
